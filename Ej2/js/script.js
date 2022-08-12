0be17600f65fe619148c703100fa461d9d7c5a74
leonlevrero <triplelevrero@gmail.com>
2022-08-12 11:07:22 -0300

    ejercicio dom

--- Ej2/js/script.js
+++ Ej2/js/script.js
@@ -1,5 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+//document.addEventListener("DOMContentLoaded") { 
   // Escribe el código necesario aquí
-  
-  //
-});
+ // var element = document.getElementById("lorem");
+  //element.addEventListener("click").classList.add("highlight");
+   // } 
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+      document.getElementById("highlight").addEventListener("click", function () {
+       // document.getElementById("lorem").classList.add("highlight");
+        document.getElementById("lorem").className="highlight";
+         });
+    
+    
+    });
